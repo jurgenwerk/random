0be41abc04c28d1d2e3295356a551ef048986ecd
Matic Jurglic <matic@jurglic.si>
2013-11-11 12:43:21 +0100

    first commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :email, :has_driver_license, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = '8757c7465f63949eb8969c812497379b2c8b003bcc2fbe79a1cdb046111f50422f3946c36f722d0a5a7866b8f14d476588670df09bdcd7becde3bc43ca3c69fb'
